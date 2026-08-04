fe0c5fd73230a5b0b236bfb008b5641e4226b5c6
robinskiii <robins38@icloud.com>
2026-08-04 22:42:40 +0200

    framework for nn architecture

--- neural_network_architecture.py
+++ neural_network_architecture.py
@@ -53,3 +53,61 @@ class Dense(Layer):
         self.bias -= learning_rate * bias_error
 
         return input_error
+
+
+
+class ReLU(Layer):
+    """
+    Activation Layer
+    """
+
+    def forward(self, input_data):
+
+        return np.maximum(0, input_data)
+
+    def backward(self, output_error, learning_rate):
+        pass                                                # TO DO
+
+
+
+class Convolution(Layer):
+    pass                                                    # TO DO
+
+
+
+class Flatten(Layer):
+    pass                                                    # TO DO
+
+
+
+class Model:
+    """
+    Neural Network composed of a sequence of Layers
+    """
+    def __init__(self):
+        self.layers = []
+
+    def add(self, layer):
+        self.layers.append(layer)
+
+    def predict(self, input_data):
+        pass                                                          # TO DO
+
+    def train(self, x_train, y_train, epochs, learning_rate):
+        pass                                                          # TO DO
+
+
+
+def loss(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+
+
+def loss_derivative(y_true, y_pred):
+    return 2 * (y_pred - y_true) / y_true.size
+
+
+
+if __name__ == "__main__":
+    # Initialize network
+    model = Model()
