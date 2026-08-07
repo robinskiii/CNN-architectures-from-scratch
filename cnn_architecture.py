da8423c4fb2a85c6d4f6eb5db4be5da13b9283a6
robinskiii <robins38@icloud.com>
2026-08-07 22:52:48 +0200

    sigmoid (binary class. works)

--- cnn_architecture.py
+++ cnn_architecture.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from collections.abc import Callable
 
 import numpy as np
 
@@ -106,11 +107,35 @@ class Flatten(Layer):
 
 
 
+class Sigmoid(Layer):
+    """
+    Sigmoid Activation Layer (binary classification)
+    """
+    input: np.ndarray
+    output: np.ndarray
+
+    def __init__(self, dim: int) -> None:
+        super().__init__(dim, dim)
+
+    def forward(self, input_data: np.ndarray) -> np.ndarray:
+        self.input = input_data
+        clipped_input = np.clip(input_data, -500, 500)
+        self.output = 1 / (1 + np.exp(-clipped_input))
+        return self.output
+
+    def backward(self, output_error: np.ndarray, learning_rate: float) -> np.ndarray:
+        derivative = self.output * (1 - self.output)
+        return output_error * derivative
+
+
+
 class Model:
     """
     Neural Network composed of a sequence of Layers
     """
     layers: list[Layer]
+    loss_function: Callable
+    loss_derivative: Callable
 
     def __init__(self) -> None:
         self.layers = []
@@ -122,6 +147,10 @@ class Model:
                 raise ValueError
 
         self.layers.append(layer)
+
+    def set_loss_function(self, loss_function: Callable, loss_derivative: Callable) -> None:
+        self.loss_function = loss_function
+        self.loss_derivative = loss_derivative
 
     def predict(self, input_data: np.ndarray) -> np.ndarray:
 
@@ -140,10 +169,10 @@ class Model:
                 output = layer.forward(output)
 
             # track the loss
-            cost = loss(y_train, output)
+            cost = self.loss_function(y_train, output)
 
             # back prop
-            error = loss_derivative(y_train, output)
+            error = self.loss_derivative(y_train, output)
             for layer in reversed(self.layers):
                 error = layer.backward(error, learning_rate)
 
@@ -151,32 +180,57 @@ class Model:
 
 
 
-def loss(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+def mse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Mean Squared Error loss function
+    """
     return np.mean(np.power(y_true - y_pred, 2))
 
-
-
-def loss_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+def mse_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     return 2 * (y_pred - y_true) / y_true.size
 
 
 
+def bce(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Binary cross entropy loss function
+    """
+    epsilon = 1e-15 # to prevent undefined log(0)
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+def bce_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    epsilon = 1e-15
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return - (y_true / y_pred) + (1 - y_true) / (1 - y_pred)
+
+
+
 if __name__ == "__main__":
+    np.random.seed(0)
+
+    batch_size = 32
+    input_dim = 4
+    output_dim = 1
+
+    # Random test data
+    X_train = np.random.randn(batch_size, input_dim)
+    y_train = np.random.randint(2, size=(batch_size, 1))
+
     # Initialize network
     model = Model()
-    model.add(Dense(2, 3))
-    model.add(ReLU(3))
-    model.add(Dense(3, 4))
-    model.add(ReLU(4))
-    model.add(Dense(4, 2))
-    model.add(ReLU(2))
-    model.add(Dense(2, 1))
+    model.add(Dense(input_dim, 10))
+    model.add(ReLU(10))
+    model.add(Dense(10, 6))
+    model.add(ReLU(6))
+    model.add(Dense(6, 5))
+    model.add(ReLU(5))
+    model.add(Dense(5, 1))
+    model.add(Sigmoid(output_dim))
 
-
-    test_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    test_labels = np.array([[0], [1], [1], [0]])
+    model.set_loss_function(bce, bce_derivative)
 
     print("\n",65*"=")
-    model.train(test_data, test_labels, epochs=50, learning_rate=0.2)
+    model.train(X_train, y_train, epochs=10000, learning_rate=0.01)
     print(65*"=","\n")
-    print(model.predict(test_data))
+    # print(model.predict(X_train))
